78a406bf6032826d065c839d5a5cf7b65aa9f9b1
unknown <aguikenna@fiberswift.com>
2022-04-04 13:51:06 +0100

    ''

--- company/urls.py
+++ company/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('',Index.as_view(),name = 'index'),
     path('about/',About.as_view(),name = 'about-us'),
     path('contact/',Contact.as_view(),name = 'contact'),
-    path('FAQ/',Faq.as_view(),name='faq'),
-    path('terms-of-service/',TOS.as_view(),name = 'legal'),    
+    #path('FAQ/',Faq.as_view(),name='faq'),
+    #path('terms-of-service/',TOS.as_view(),name = 'legal'),    
     
 ]
