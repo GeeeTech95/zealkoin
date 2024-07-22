f5ccbd6c9eeced8667febf7bb2d261da38e5e7e7
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2024-07-22 09:49:09 +0100

    ''

--- Users/models.py
+++ Users/models.py
@@ -13,8 +13,8 @@ import random
 
 
 class Country(models.Model) :
-    name = models.CharField(max_length=20)
-    short_name = models.CharField(max_length=5)
+    name = models.CharField(max_length=50)
+    short_name = models.CharField(max_length=10)
 
     def __str__(self) :
         return "{}({})".format(self.name,self.short_name)
